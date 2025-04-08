1ca4929f84e40a13dc5a7689c1a3eef7dfa888ac
dheerubaba123 <k.dheerajbaba2005@gmail.com>
2025-04-08 11:54:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
-// Serve static files
 app.use(express.static(__dirname));
 
-// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -25,12 +22,10 @@ app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, 'signup.html'));
 });
 
-// Handle 404
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
-// Start server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
